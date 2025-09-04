b6a8a2ba0d08194085878a848a58e82772632afc
lailai4 <52186241+lailai4@users.noreply.github.com>
2025-09-04 18:45:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: '/CoinView/',    // 👈 关键：GitHub Pages 的子路径
   plugins: [react()],
   server: { host: true, port: 5173 }
 })
